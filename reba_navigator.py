5adeb902ac2663735d201145530ba451df5902b5
Jack Ogozaly <72467438+JackOgozaly@users.noreply.github.com>
2023-12-19 15:51:13 -0500

    Update reba_navigator.py

--- reba_navigator.py
+++ reba_navigator.py
@@ -244,8 +244,7 @@ authenticator = stauth.Authenticate(
     config['credentials'],
     config['cookie']['name'],
     config['cookie']['key'],
-    config['cookie']['expiry_days'],
-)
+    config['cookie']['expiry_days'])
 
 name, authentication_status, username = authenticator.login('Login', 'main')
 
